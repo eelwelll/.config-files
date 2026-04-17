4561040850fc64a77c6d0e4c5aae5846a3fb70a2
eelwelll <eelwell.07@gmail.com>
2026-04-17 12:24:08 +0100

    finally fixed it to make the mediaArt.sh run

--- waybar/scripts/mediaArt.sh
+++ waybar/scripts/mediaArt.sh
@@ -4,5 +4,5 @@ TMP="/tmp/coverimage.png"
 metadata=$(/usr/bin/playerctl metadata --format '{{mpris:artUrl}}' 2>/dev/null)
 if [ -n "$metadata" ]; then
     /usr/bin/curl -s "$metadata" -o "$TMP"
-    magick "$TMP" "$OUTPUT"
+    magick "$metadata" "$OUTPUT"
 fi
