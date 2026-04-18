e012a8b88558f14a616cd23135888f085c54f377
eelwelll <eelwell.07@gmail.com>
2026-04-18 01:43:30 +0100

    SOUNDBAR to make it look really really cool

--- waybar/scripts/getTimePercent.sh
+++ waybar/scripts/getTimePercent.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+lengths=("﹏         " "﹏﹏        " "﹏﹏﹏       " "﹏﹏﹏﹏      " "﹏﹏﹏﹏﹏     " "﹏﹏﹏﹏﹏﹏    " "﹏﹏﹏﹏﹏﹏﹏  " "﹏﹏﹏﹏﹏﹏﹏﹏  " "﹏﹏﹏﹏﹏﹏﹏﹏﹏ " "﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏")
 duration=$(playerctl position --format "{{ duration(position) }}")
 length=$(playerctl metadata --format "{{duration(mpris:length)}}")
 minD=${duration%:*}
@@ -7,19 +8,10 @@ secD=${duration#*:}
 minL=${length%:*}
 secL=${length#*:}
 
-#totalRemainingMinutes=$(echo "scale=2; $minD / $minL" | bc)
-#if [[ $secL -lt $secD ]]; then
-#  totalRemainingSeconds=$(echo "scale=2; $secL / $secD" | bc)
-#else
-#  totalRemainingSeconds=$(echo "scale=2; $secD / $secL" | bc)
-#fi
-#totalRemainingSeconds=$(echo "$totalRemainingSeconds * 10" | bc)
-#totalRemainingMinutes=$(echo "$totalRemainingMinutes * 100" | bc)
-
-#totalPercent=$(echo "$totalRemainingMinutes + $totalRemainingSeconds" | bc)
-
 totalsecondsL=$((($minL*60)+$secL))
 totalSecondsD=$((($minD*60)+$secD))
 percent=$(echo "scale=2; $totalSecondsD / $totalsecondsL" | bc)
-percent=$(echo "$percent * 100" | bc)
-echo $percent
+percent=$(echo "$percent * 10" | bc)
+# echo ${percent%.*}
+final=${lengths[${percent%.*}]}
+echo $final 
